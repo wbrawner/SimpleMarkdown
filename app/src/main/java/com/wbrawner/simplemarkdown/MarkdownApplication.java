cd564ad36314811b3e3e01a686e3cde3b3872b20
Billy Brawner <billybrawner@gmail.com>
2017-11-28 20:11:15 -0600

    Update gradle and add crashlytics for release builds

--- app/src/main/java/com/wbrawner/simplemarkdown/MarkdownApplication.java
+++ app/src/main/java/com/wbrawner/simplemarkdown/MarkdownApplication.java
@@ -1,6 +1,8 @@
 package com.wbrawner.simplemarkdown;
 
 import android.app.Application;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 /**
  * Created by billy on 8/22/17.
@@ -13,6 +15,9 @@ public class MarkdownApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        if (!BuildConfig.DEBUG) {
+            Fabric.with(this, new Crashlytics());
+        }
         component = DaggerAppComponent.builder()
                 .appModule(new AppModule())
                 .build();
