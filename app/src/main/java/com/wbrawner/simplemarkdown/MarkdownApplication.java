0e2841a0e27b159ec47979fd368488e921ad4ef3
Billy Brawner <billybrawner@gmail.com>
2018-09-30 19:53:03 -0500

    Replace ACRA with Firebase Crashlytics

--- app/src/main/java/com/wbrawner/simplemarkdown/MarkdownApplication.java
+++ app/src/main/java/com/wbrawner/simplemarkdown/MarkdownApplication.java
@@ -1,23 +1,7 @@
 package com.wbrawner.simplemarkdown;
 
 import android.app.Application;
-import android.content.Context;
 
-import org.acra.ACRA;
-import org.acra.annotation.AcraCore;
-import org.acra.annotation.AcraHttpSender;
-import org.acra.data.StringFormat;
-import org.acra.sender.HttpSender;
-
-import static com.wbrawner.simplemarkdown.BuildConfig.ACRA_PASS;
-import static com.wbrawner.simplemarkdown.BuildConfig.ACRA_URL;
-import static com.wbrawner.simplemarkdown.BuildConfig.ACRA_USER;
-
-@AcraCore(buildConfigClass = BuildConfig.class, reportFormat = StringFormat.JSON)
-@AcraHttpSender(uri = ACRA_URL,
-        basicAuthLogin = ACRA_USER,
-        basicAuthPassword = ACRA_PASS,
-        httpMethod = HttpSender.Method.POST)
 public class MarkdownApplication extends Application {
 
     private AppComponent component;
@@ -32,13 +16,5 @@ public class MarkdownApplication extends Application {
 
     public AppComponent getComponent() {
         return component;
-    }
-
-    @Override
-    protected void attachBaseContext(Context base) {
-        super.attachBaseContext(base);
-
-        // The following line triggers the initialization of ACRA
-        ACRA.init(this);
     }
 }
