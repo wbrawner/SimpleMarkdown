15eb0812c045c98ca7d358aaf83f959afc31c45f
Billy Brawner <billybrawner@gmail.com>
2018-10-15 21:22:49 -0500

    Add privacy policy and limit the data sent by ACRA

--- app/src/main/java/com/wbrawner/simplemarkdown/MarkdownApplication.java
+++ app/src/main/java/com/wbrawner/simplemarkdown/MarkdownApplication.java
@@ -12,8 +12,51 @@ import org.acra.sender.HttpSender;
 import static com.wbrawner.simplemarkdown.BuildConfig.ACRA_PASS;
 import static com.wbrawner.simplemarkdown.BuildConfig.ACRA_URL;
 import static com.wbrawner.simplemarkdown.BuildConfig.ACRA_USER;
+import static org.acra.ReportField.ANDROID_VERSION;
+import static org.acra.ReportField.APPLICATION_LOG;
+import static org.acra.ReportField.APP_VERSION_CODE;
+import static org.acra.ReportField.APP_VERSION_NAME;
+import static org.acra.ReportField.AVAILABLE_MEM_SIZE;
+import static org.acra.ReportField.BRAND;
+import static org.acra.ReportField.BUILD_CONFIG;
+import static org.acra.ReportField.CRASH_CONFIGURATION;
+import static org.acra.ReportField.CUSTOM_DATA;
+import static org.acra.ReportField.INITIAL_CONFIGURATION;
+import static org.acra.ReportField.PACKAGE_NAME;
+import static org.acra.ReportField.PHONE_MODEL;
+import static org.acra.ReportField.SHARED_PREFERENCES;
+import static org.acra.ReportField.STACK_TRACE;
+import static org.acra.ReportField.STACK_TRACE_HASH;
+import static org.acra.ReportField.THREAD_DETAILS;
+import static org.acra.ReportField.TOTAL_MEM_SIZE;
+import static org.acra.ReportField.USER_APP_START_DATE;
+import static org.acra.ReportField.USER_CRASH_DATE;
 
-@AcraCore(buildConfigClass = BuildConfig.class, reportFormat = StringFormat.JSON)
+@AcraCore(
+        buildConfigClass = BuildConfig.class,
+        reportContent = {
+                ANDROID_VERSION,
+                APP_VERSION_CODE,
+                APP_VERSION_NAME,
+                APPLICATION_LOG,
+                AVAILABLE_MEM_SIZE,
+                BRAND,
+                BUILD_CONFIG,
+                CRASH_CONFIGURATION,
+                CUSTOM_DATA, // Not currently used, but might be useful in the future
+                INITIAL_CONFIGURATION,
+                PACKAGE_NAME,
+                PHONE_MODEL,
+                SHARED_PREFERENCES,
+                STACK_TRACE,
+                STACK_TRACE_HASH,
+                THREAD_DETAILS,
+                TOTAL_MEM_SIZE,
+                USER_APP_START_DATE,
+                USER_CRASH_DATE,
+        },
+        reportFormat = StringFormat.JSON
+)
 @AcraHttpSender(uri = ACRA_URL,
         basicAuthLogin = ACRA_USER,
         basicAuthPassword = ACRA_PASS,
