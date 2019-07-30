c734bc884c4c4594bf481dc98a024abeff6d19d9
Billy Brawner <billybrawner@gmail.com>
2019-07-29 19:10:13 -0700

    Add performance measurement for readability

--- app/src/main/java/com/wbrawner/simplemarkdown/utility/ReadabilityObserver.java
+++ app/src/main/java/com/wbrawner/simplemarkdown/utility/ReadabilityObserver.java
@@ -3,10 +3,13 @@ package com.wbrawner.simplemarkdown.utility;
 import android.graphics.Color;
 import android.text.SpannableString;
 import android.text.style.BackgroundColorSpan;
+import android.util.Log;
 import android.widget.EditText;
 import android.widget.TextView;
+
 import com.wbrawner.simplemarkdown.model.Readability;
 import com.wbrawner.simplemarkdown.model.Sentence;
+
 import io.reactivex.Observer;
 import io.reactivex.disposables.Disposable;
 
@@ -23,27 +26,23 @@ public class ReadabilityObserver implements Observer<String> {
 
     }
 
-
     @Override
     public void onNext(String markdown) {
-        if(markdown.length() < 1) return;
-        if(previousValue.equals(markdown)) return;
-
+        long start = System.currentTimeMillis();
+        if (markdown.length() < 1) return;
+        if (previousValue.equals(markdown)) return;
         Readability readability = new Readability(markdown);
-
         SpannableString span = new SpannableString(markdown);
-
-        for(Sentence sentence : readability.sentences()){
+        for (Sentence sentence : readability.sentences()) {
             int color = Color.TRANSPARENT;
-            if(sentence.syllableCount() > 25) color = Color.argb(100,229,232,42);
-            if(sentence.syllableCount() > 35) color = Color.argb(100,193,66,66);
+            if (sentence.syllableCount() > 25) color = Color.argb(100, 229, 232, 42);
+            if (sentence.syllableCount() > 35) color = Color.argb(100, 193, 66, 66);
             span.setSpan(new BackgroundColorSpan(color), sentence.start(), sentence.end(), 0);
         }
-
-
         text.setTextKeepState(span, TextView.BufferType.SPANNABLE);
         previousValue = markdown;
-
+        long timeTakenMs = System.currentTimeMillis() - start;
+        Log.d("SimpleMarkdown", "Handled markdown in " + timeTakenMs + "ms");
     }
 
     @Override
