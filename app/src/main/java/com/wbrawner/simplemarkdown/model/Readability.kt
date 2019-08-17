787c044da6fc5680e46314db45e6331151201c8f
Billy Brawner <billy@wbrawner.com>
2019-08-17 13:28:00 -0500

    Convert Readability to Kotlin

---
+++ app/src/main/java/com/wbrawner/simplemarkdown/model/Readability.kt
@@ -0,0 +1,29 @@
+package com.wbrawner.simplemarkdown.model
+
+import java.util.*
+
+class Readability(private val content: String) {
+
+    fun sentences(): List<Sentence> {
+        val list = ArrayList<Sentence>()
+        var startOfSentance = 0
+        var lineBuilder = StringBuilder()
+        for (i in 0 until content.length) {
+            val c = content[i] + ""
+            if (DELIMS.contains(c)) {
+                list.add(Sentence(content, startOfSentance, i))
+                startOfSentance = i + 1
+                lineBuilder = StringBuilder()
+            } else {
+                lineBuilder.append(c)
+            }
+        }
+        val line = lineBuilder.toString()
+        if (line.isNotEmpty()) list.add(Sentence(content, startOfSentance, content.length))
+        return list
+    }
+
+    companion object {
+        private const val DELIMS = ".!?\n"
+    }
+}
