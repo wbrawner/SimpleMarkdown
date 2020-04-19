5c04dd0e8f37a5d24322bbc509bfe6a1b17b7e1b
William Brawner <me@wbrawner.com>
2020-04-19 08:50:24 -0700

    Replace Crashlytics with ACRA
    Signed-off-by: William Brawner <me@wbrawner.com>

--- app/src/main/java/com/wbrawner/simplemarkdown/utility/ErrorHandler.kt
+++ app/src/main/java/com/wbrawner/simplemarkdown/utility/ErrorHandler.kt
@@ -1,33 +1,56 @@
 package com.wbrawner.simplemarkdown.utility
 
-import android.content.Context
+import android.app.Application
 import android.util.Log
-import com.crashlytics.android.Crashlytics
+import com.evernote.android.job.JobRequest
 import com.wbrawner.simplemarkdown.BuildConfig
-import io.fabric.sdk.android.Fabric
+import org.acra.ACRA
+import org.acra.config.CoreConfigurationBuilder
+import org.acra.config.HttpSenderConfigurationBuilder
+import org.acra.config.SchedulerConfigurationBuilder
+import org.acra.data.StringFormat
+import org.acra.sender.HttpSender
 import java.util.concurrent.atomic.AtomicBoolean
 
 interface ErrorHandler {
-    fun init(context: Context)
+    fun init(application: Application)
     fun reportException(t: Throwable, message: String? = null)
 }
 
-class CrashlyticsErrorHandler : ErrorHandler {
+class AcraErrorHandler : ErrorHandler {
     private val isInitialized = AtomicBoolean(false)
 
-    override fun init(context: Context) {
+    override fun init(application: Application) {
+        if (BuildConfig.ACRA_URL.isBlank()
+                || BuildConfig.ACRA_USER.isBlank()
+                || BuildConfig.ACRA_PASS.isBlank()) {
+            return
+        }
         if (!isInitialized.getAndSet(true)) {
-            Fabric.with(context, Crashlytics())
+            val builder = CoreConfigurationBuilder(application)
+                    .setBuildConfigClass(BuildConfig::class.java)
+                    .setReportFormat(StringFormat.JSON)
+            builder.getPluginConfigurationBuilder(HttpSenderConfigurationBuilder::class.java)
+                    .setUri(BuildConfig.ACRA_URL)
+                    .setHttpMethod(HttpSender.Method.POST)
+                    .setBasicAuthLogin(BuildConfig.ACRA_USER)
+                    .setBasicAuthPassword(BuildConfig.ACRA_PASS)
+                    .setEnabled(true)
+            builder.getPluginConfigurationBuilder(SchedulerConfigurationBuilder::class.java)
+                    .setRequiresNetworkType(JobRequest.NetworkType.UNMETERED)
+                    .setRequiresBatteryNotLow(true)
+                    .setEnabled(true)
+            ACRA.init(application, builder)
         }
     }
 
     override fun reportException(t: Throwable, message: String?) {
         @Suppress("ConstantConditionIf")
         if (BuildConfig.DEBUG) {
-            Log.e("CrashlyticsErrorHandler", "Caught exception: $message", t)
+            Log.e("AcraErrorHandler", "Caught exception: $message", t)
             return
         }
         if (!isInitialized.get()) return
-        Crashlytics.logException(t)
+        ACRA.getErrorReporter().handleException(t)
     }
 }
