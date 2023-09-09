55755613e7e0125fa9a90a008b2ae12039152609
William Brawner <me@wbrawner.com>
2023-09-08 20:28:27 -0600

    Bump Kotlin to 1.9.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ buildscript {
         classpath("com.android.tools.build:gradle:8.1.1")
         classpath("com.google.gms:google-services:4.3.15")
         classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.4")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.20")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.10")
         classpath("com.osacky.flank.gradle:fladle:0.17.4")
     }
 }
