92123d2f2488cfc735c9e8d2c8a60175123f666b
William Brawner <me@wbrawner.com>
2024-05-18 22:30:55 -0600

    Address buildHealth issues

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -52,15 +52,9 @@ android {
 
 dependencies {
     val acraVersion = "5.11.3"
-    api("ch.acra:acra-core:$acraVersion")
+    implementation("ch.acra:acra-core:$acraVersion")
     implementation("ch.acra:acra-http:$acraVersion")
-    implementation("ch.acra:acra-limiter:$acraVersion")
-    implementation("ch.acra:acra-advanced-scheduler:$acraVersion")
+    runtimeOnly("ch.acra:acra-limiter:$acraVersion")
+    runtimeOnly("ch.acra:acra-advanced-scheduler:$acraVersion")
     api("com.jakewharton.timber:timber:5.0.1")
-    implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.12.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
--- free/build.gradle.kts
+++ free/build.gradle.kts
@@ -39,25 +39,9 @@ android {
 }
 
 dependencies {
-    api(project(":core"))
-    implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.12.0")
     implementation("com.jakewharton.timber:timber:5.0.1")
     val composeBom = platform("androidx.compose:compose-bom:2023.08.00")
     implementation(composeBom)
     androidTestImplementation(composeBom)
     implementation("androidx.compose.runtime:runtime")
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.activity:activity-compose")
-    implementation("androidx.compose.foundation:foundation")
-    implementation("androidx.compose.foundation:foundation-layout")
-    implementation("androidx.compose.material:material")
-    implementation("androidx.compose.runtime:runtime-livedata")
-    implementation("androidx.compose.ui:ui-tooling")
-    implementation("androidx.compose.material3:material3")
-    implementation("androidx.compose.material:material-icons-extended")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
