51e56524754ca42e8a4686330f6da272105643b0
William Brawner <me@wbrawner.com>
2024-08-08 18:59:09 -0600

    Move changelog task to standalone file

--- buildSrc/src/main/kotlin/com/wbrawner/releasehelper/ReleaseHelperPlugin.kt
+++ buildSrc/src/main/kotlin/com/wbrawner/releasehelper/ReleaseHelperPlugin.kt
@@ -2,46 +2,15 @@ package com.wbrawner.releasehelper
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.tasks.Exec
 import org.gradle.kotlin.dsl.extra
 import org.gradle.kotlin.dsl.provideDelegate
-import java.io.ByteArrayOutputStream
+import org.gradle.kotlin.dsl.register
 import java.io.File
+
 
 class ReleaseHelperPlugin : Plugin<Project> {
     override fun apply(target: Project) {
-        target.tasks.register("getLatestTag", Exec::class.java) {
-            val latestTag = ByteArrayOutputStream()
-            standardOutput = latestTag
-            commandLine("git describe --tags --abbrev=0".split(" "))
-            doLast {
-                target.project.extra["latestTag"] = latestTag.toString().trim()
-                logger.info("Latest tag: ${target.project.extra["latestTag"]}")
-            }
-        }
-
-        target.tasks.register("changelog") {
-            val changelogFile = File(target.projectDir, "src/play/play/release-notes/en-US/default.txt")
-            inputs.property("tag", target.provider {
-                target.project.extra["latestTag"]
-            })
-            outputs.file(changelogFile)
-            dependsOn("getLatestTag")
-            doLast {
-                val latestTag: String by target.project.extra
-                val changelog = ByteArrayOutputStream()
-                target.exec {
-                    standardOutput = changelog
-                    commandLine = "git log --format=\"%B\" ${latestTag.trim()}..".split(" ")
-                }
-                changelogFile.writeText(
-                        changelog.toString()
-                                .split("\n")
-                                .mapNotNull { it.trim('"').ifBlank { null } }
-                                .joinToString("\n") { "- $it" }
-                )
-            }
-        }
+        target.tasks.register<ChangelogTask>("changelog")
 
         target.tasks.register("majorRelease") {
             dependsOn("changelog", "getLatestTag")
