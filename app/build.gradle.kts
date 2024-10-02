79d609f1383014453bd6d79f84d0ef11de315370
William Brawner <me@wbrawner.com>
2024-10-01 21:17:34 -0600

    Bump Java version to 11

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,11 +37,11 @@ android {
     }
     compileSdk = libs.versions.maxSdk.get().toInt()
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "11"
     }
     defaultConfig {
         applicationId = "com.wbrawner.simplemarkdown"
--- non-free/build.gradle.kts
+++ non-free/build.gradle.kts
@@ -24,14 +24,14 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
     buildFeatures {
         compose = true
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "11"
     }
     lint {
         disable += listOf("AndroidGradlePluginVersion", "GradleDependency")
