80c4e3d52de00e9c06f637718db30b0f0fe309fe
William Brawner <me@wbrawner.com>
2025-09-05 20:59:27 +0200

    Bump version for release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,8 +50,8 @@ android {
         applicationId = "com.wbrawner.simplemarkdown"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.maxSdk.get().toInt()
-        versionCode = 53
-        versionName = "2025.7.0"
+        versionCode = 54
+        versionName = "2025.09.00"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         testInstrumentationRunnerArguments["clearPackageData"] = "true"
         buildConfigField("boolean", "ENABLE_CUSTOM_CSS", "true")
