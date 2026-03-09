df57d4a20a9c7775b5f8ed5a901922bd60d634c6
William Brawner <me@wbrawner.com>
2026-03-08 23:01:58 -0600

    Fix failing UI tests

--- app/src/androidTest/kotlin/com/wbrawner/simplemarkdown/robot/TopAppBarRobot.kt
+++ app/src/androidTest/kotlin/com/wbrawner/simplemarkdown/robot/TopAppBarRobot.kt
@@ -3,6 +3,7 @@ package com.wbrawner.simplemarkdown.robot
 import androidx.compose.ui.test.SemanticsNodeInteraction
 import androidx.compose.ui.test.hasAnySibling
 import androidx.compose.ui.test.hasContentDescription
+import androidx.compose.ui.test.hasParent
 import androidx.compose.ui.test.hasText
 import androidx.compose.ui.test.junit4.ComposeTestRule
 import com.wbrawner.simplemarkdown.waitUntilIsDisplayed
@@ -14,9 +15,10 @@ interface TopAppBarRobot {
 class ComposeTopAppBarRobot(private val composeTestRule: ComposeTestRule) : TopAppBarRobot {
     override fun checkTitleEquals(title: String) =
         composeTestRule.onNode(
-            hasAnySibling(
-                hasContentDescription("Main Menu") or hasContentDescription(
-                    "Back"
+            hasParent(
+                hasAnySibling(
+                    hasContentDescription("Main Menu")
+                            or hasContentDescription("Back")
                 )
             ).and(hasText(title))
         )
